0816e2e0c0edf0716cb38916f321416486a081e1
Magistra <magistraagis@gmail.com>
2023-05-05 17:38:29 +0800

    fix book container interfaces

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @include('partials.head')
         <div class="container">
             @include('partials.navbar')
-            <div class="container bg-light mt-5 p-3">
+            <div class="container bg-light mt-5 p-3 " >
                 <div class="row rounded-lg">
                     <div class="col">
                         <h1 class="text-center">
@@ -9,16 +9,18 @@
                         </h1>
                     </div>
                 </div>
-                <div class="row rounded-lg">
-                    <div class="col">
-                       @foreach ($buku as $data )
-                           <ul>
-                                <li>{{ $data->nama }}</li>
-                                <li>{{ $data->pengarang }}</li>
-                                <li>{{ $data->harga }}</li>
-                           </ul>
-                       @endforeach
-                    </div>
+                <div class="row d-flex justify-content-center border">
+                    @foreach ($buku as $data )
+                        <div class="col-8 p-4 bg-primary text-white rounded-3 d-flex items-center justify-content-between">
+                            <div>
+                                <h3>{{ $data->nama }}</h3>
+                                <p>{{ $data->pengarang }}</p>
+                            </div>
+                            <div>
+                                <h5>Rp.{{ $data->harga }}</h5>
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>

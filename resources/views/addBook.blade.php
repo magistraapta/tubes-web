1f95a50bea846b948eff1d372066ca333c8de795
Magistra <magistraagis@gmail.com>
2023-05-07 20:37:25 +0800

    add image input

--- resources/views/addBook.blade.php
+++ resources/views/addBook.blade.php
@@ -2,7 +2,7 @@
 <div class="container">
     @include('partials.navbar')
     <div class="container bg-light mt-5 d-flex justify-content-center">
-        <form class="col-6 p-5" id="bookForm" method="POST" action="store">
+        <form class="col-6 p-5" id="bookForm" method="POST" action="store" enctype="multipart/form-data">
             @csrf
             <div class="mb-3">
                 <label for="bookDesc">Nama</label>
@@ -13,8 +13,16 @@
                 <input type="text" name="pengarang" class="form-control">
             </div>
             <div class="mb-3">
+                <label for="bookDesc">Kategori</label>
+                <input type="text" name="Kategori" class="form-control">
+            </div>
+            <div class="mb-3">
                 <label for="bookDesc">Harga</label>
                 <input type="number" name="harga" class="form-control">
+            </div>
+            <div class="mb-3">
+                <label for="bookDesc">Cover buku</label>
+                <input type="file" name="foto" class="form-control">
             </div>
             <div class="mb-3">
                 <label for="bookDesc">Deskripsi</label>

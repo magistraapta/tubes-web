d0128af76c8c1eab8d8ec9bc528a730dba7405b0
Magistra <magistraagis@gmail.com>
2023-05-07 20:37:51 +0800

    add image input

--- resources/views/editBook.blade.php
+++ resources/views/editBook.blade.php
@@ -7,19 +7,27 @@
             @csrf
             <div class="mb-3">
                 <label for="bookDesc">Nama</label>
-                <input type="text" name="nama" class="form-control">
+                <input type="text" name="nama" class="form-control" value="{{ $buku->nama }}">
             </div>
             <div class="mb-3">
                 <label for="bookDesc">Pengarang</label>
-                <input type="text" name="pengarang" class="form-control">
+                <input type="text" name="pengarang" class="form-control" value="{{ $buku->pengarang }}">
+            </div>
+            <div class="mb-3">
+                <label for="bookDesc">Kategori</label>
+                <input type="text" name="pengarang" class="form-control" value="{{ $buku->kategori }}">
             </div>
             <div class="mb-3">
                 <label for="bookDesc">Harga</label>
-                <input type="number" name="harga" class="form-control">
+                <input type="number" name="harga" class="form-control" value="{{ $buku->harga }}">
+            </div>
+            <div class="mb-3">
+                <label for="bookDesc">Cover buku</label>
+                <input type="file" name="foto" class="form-control" value="{{ $buku->foto }}">
             </div>
             <div class="mb-3">
                 <label for="bookDesc">Deskripsi</label>
-                <textarea name="deskripsi" id="bookDesc" form="bookForm" cols="61" rows="7"></textarea>
+                <textarea name="deskripsi" id="bookDesc" form="bookForm" cols="61" rows="7">{{ $buku->deskripsi}}</textarea>
             </div>
             
             <button type="submit" class="btn btn-primary" value="create">Submit</button>

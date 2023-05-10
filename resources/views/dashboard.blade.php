297c5bb48f75b4da81593bfd4aefd3c72bfa90e3
Magistra <magistraagis@gmail.com>
2023-05-10 15:32:39 +0800

    add dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,10 @@
+@include('partials.head')
+@include('partials.navbar')
+        <div class="container border">
+            
+            <h1>welcome to dashboard</h1>
+        </div>
+@include('partials.footer')
+
+
+        
